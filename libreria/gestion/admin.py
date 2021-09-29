35ffac0961f8fbcfdd50f8984562f9e7f0a05d0a
fernando0313 <rojofernando296@gmail.com>
2021-09-28 20:46:10 -0500

    subida

---
+++ libreria/gestion/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import ClienteModel, ProductoModel
+
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ['productoId','productoNombre','productoPrecio']
+    search_fields = ['productoNombre','productoUnidadMedida']
+    list_filter = ['productoUnidadMedida']
+    readonly_fields = ['productoId']
+admin.site.register(ClienteModel)
+admin.site.register(ProductoModel, ProductoAdmin)
